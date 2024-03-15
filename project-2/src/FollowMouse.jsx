9f22f8101a52d50f8023c6d99934037e15351b7a
moiseslugo_04 <gameover96538@gmail.com>
2024-03-15 05:18:30 -0300

    udapte repo with three project

---
+++ project-2/src/FollowMouse.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+export const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: -205, y: -205 });
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    };
+
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    //clear
+    //cuando el componente se desmonta
+    //cunado canbian las dependencias
+    return () => {
+      console.log("clear");
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "2px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "activar"} seguir puntero
+      </button>
+    </>
+  );
+};
